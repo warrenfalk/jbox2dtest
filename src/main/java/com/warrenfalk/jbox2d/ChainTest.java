260ba1dbd16eb88385c9373ae94163dcd38a0539
Warren Falk <warren@warrenfalk.com>
2013-07-25 19:45:36 -0400

    add base and weight to chain plus obstacle

--- src/main/java/com/warrenfalk/jbox2d/ChainTest.java
+++ src/main/java/com/warrenfalk/jbox2d/ChainTest.java
@@ -1,6 +1,7 @@
 package com.warrenfalk.jbox2d;
 
 import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
@@ -17,20 +18,27 @@ public class ChainTest extends TestbedTest {
 		//getWorld().setGravity(new Vec2());
 
 		CircleShape circle = new CircleShape();
-		circle.m_radius = 0.4f;
+		circle.m_radius = 0.2f;
 		
 		BodyDef bd = new BodyDef();
 		bd.type = BodyType.DYNAMIC;
 		
+		BodyDef fixed = new BodyDef();
+		fixed.type = BodyType.STATIC;
 		
-		Body prev = null;
-		for (int i = 0; i < 15; i++) {
-			FixtureDef fd = new FixtureDef();
-			fd.shape = circle;
-			fd.density = 1.0f;
-			fd.friction = 0.9f;
+		FixtureDef fd = new FixtureDef();
+		fd.shape = circle;
+		fd.density = 2.0f;
+		fd.friction = 0.9f;
+		
+		fixed.position.set(-10.2f, 0.35f);
+		Body base = m_world.createBody(fixed);
+		base.createFixture(fd);
+		
+		Body prev = base;
+		for (int i = 0; i < 30; i++) {
 			
-			bd.position.set(-9f + 1.2f * i, 0.35f);
+			bd.position.set(-9f + 0.6f * i, 0.35f);
 			Body link = m_world.createBody(bd);
 			link.createFixture(fd);
 			
@@ -42,6 +50,28 @@ public class ChainTest extends TestbedTest {
 			
 			prev = link;
 		}
+		
+		bd.position.set(10.2f, 0.35f);
+		fd.density = 100f;
+		Body weight = m_world.createBody(bd);
+		weight.createFixture(fd);
+		
+		DistanceJointDef djd = new DistanceJointDef();
+		djd.initialize(prev, weight, prev.getPosition(), weight.getPosition());
+		getWorld().createJoint(djd);
+		
+		PolygonShape ps = new PolygonShape();
+		ps.setAsBox(2.5f, 0.8f);
+
+		fixed.position.set(3f, -4f);
+		Body block = getWorld().createBody(fixed);
+		
+		fd.shape = ps;
+		block.createFixture(fd);
+		
+		
+		
+
 	}
 
 	@Override
