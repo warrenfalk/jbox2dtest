52c8a3ef15d06f810628a110d69c957ddbac4f5b
Warren Falk <warren@warrenfalk.com>
2013-07-28 15:14:39 -0400

    begin implementation of a shootRope() function

--- src/main/java/com/warrenfalk/jbox2d/RopeTest.java
+++ src/main/java/com/warrenfalk/jbox2d/RopeTest.java
@@ -1,10 +1,14 @@
 package com.warrenfalk.jbox2d;
 
+import java.util.LinkedList;
+
+import org.jbox2d.callbacks.RayCastCallback;
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.Fixture;
 import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 import org.jbox2d.dynamics.joints.Joint;
@@ -15,8 +19,8 @@ import org.jbox2d.testbed.framework.TestbedTest;
 public class RopeTest extends TestbedTest {
 	
 	class Rope {
-		public Body[] segments;
-		public Joint[] joints;
+		public LinkedList<Body> segments = new LinkedList<Body>();
+		public LinkedList<Joint> joints = new LinkedList<Joint>();
 	}
 	
 	Rope rope;
@@ -74,8 +78,14 @@ public class RopeTest extends TestbedTest {
 	public void keyPressed(char keyChar, int keyCode) {
 		switch (keyChar) {
 		case 'j':
-			if (rope == null)
-				rope = createRope(getWorld(), thickness, resolution, base, weight, new Vec2(0, 0), weight.getWorldPoint(new Vec2(3f, 0f)));
+			if (rope == null) {
+				//rope = createRope(getWorld(), thickness, resolution, base, weight, new Vec2(0, 0), weight.getWorldPoint(new Vec2(3f, 0f)));
+				Vec2 localFrom = new Vec2(3f, 0f);
+				Vec2 worldFrom = weight.getWorldPoint(localFrom);
+				Vec2 ray = new Vec2(0, 0).sub(worldFrom);
+				float angle = (float)Math.atan2(ray.y, ray.x);
+				rope = shootRope(getWorld(), null, thickness, resolution, weight, localFrom, angle);
+			}
 			else {
 				for (Joint joint : rope.joints)
 					if (joint != null)
@@ -86,7 +96,39 @@ public class RopeTest extends TestbedTest {
 			}
 		}
 	}
+	
+	static class RopeRaycast implements RayCastCallback {
+		Fixture fixture;
+		Vec2 strikePoint;
+		Vec2 normal;
+		float fraction;
+		
+		public RopeRaycast(World world, Vec2 from, float angle, float maxLength) {
+			Vec2 to = from.add(new Vec2((float)Math.cos(angle), (float)Math.sin(angle)).mul(17f));
+			world.raycast(this, from, to);
+			if (strikePoint == null)
+				strikePoint = to;
+		}
 
+		@Override
+		public float reportFixture(Fixture fixture, Vec2 point, Vec2 normal, float fraction) {
+			this.fixture = fixture;
+			this.strikePoint = point;
+			this.normal = normal;
+			this.fraction = fraction;
+			return fraction;
+		}
+	}
+	
+	private Rope shootRope(World world, Rope rope, float thickness, float resolution, Body fromBody, Vec2 localFrom, float angle) {
+		// for now this is instant, find a place for the rope to strike, then strike there.
+		// find a strike point for the rope
+		Vec2 from = fromBody.getWorldPoint(localFrom);
+		RopeRaycast rrc = new RopeRaycast(world, from, angle, 60f);
+		
+		return createRope(world, thickness, resolution, fromBody, rrc.fixture.getBody(), from, rrc.strikePoint);
+	}
+	
 	private Rope createRope(World world, float thickness, float resolution, Body fromBody, Body toBody, Vec2 from, Vec2 to) {
 		Rope rope = new Rope();
 		
@@ -94,9 +136,6 @@ public class RopeTest extends TestbedTest {
 		float length = vector.length();
 		int count = (int)Math.floor(length / resolution);
 		resolution = length / (float)count;
-		
-		rope.segments = new Body[count];
-		rope.joints = new Joint[count * 2 + 2];
 		
 		Vec2 direction = new Vec2(vector);
 		direction.normalize();
@@ -122,15 +161,16 @@ public class RopeTest extends TestbedTest {
 		int i;
 		Body prev = fromBody;
 		for (i = 0; i < count; i++) {
-			segmentBodyDef.position = direction.mul(resolution * (i + 0.5f));
+			segmentBodyDef.position = from.add(direction.mul(resolution * (i + 0.5f)));
 			
-			Body segment = rope.segments[i] = world.createBody(segmentBodyDef);
+			Body segment = world.createBody(segmentBodyDef);
+			rope.segments.addLast(segment);
 			segment.createFixture(segmentFixture);
 			
 			RevoluteJointDef jd = new RevoluteJointDef();
-			jd.initialize(segment, prev, direction.mul(resolution * i));
+			jd.initialize(segment, prev, from.add(direction.mul(resolution * i)));
 			jd.collideConnected = false;
-			rope.joints[i] = world.createJoint(jd);
+			rope.joints.addLast(world.createJoint(jd));
 			
 			if (i % 2 == 1) {
 				// create a rope joint from each body to every other segment
@@ -143,21 +183,21 @@ public class RopeTest extends TestbedTest {
 				rj.bodyB = fromBody;
 				rj.localAnchorB.set(fromBody.getLocalPoint(from));
 				rj.maxLength = rj.bodyA.getWorldPoint(rj.localAnchorA).sub(from).length();
-				rope.joints[i + count] = world.createJoint(rj);
+				rope.joints.addLast(world.createJoint(rj));
 				
 				rj.bodyB = toBody;
 				rj.localAnchorB.set(toBody.getLocalPoint(to));
 				rj.maxLength = rj.bodyA.getWorldPoint(rj.localAnchorA).sub(to).length();
-				rope.joints[i + count - 1] = world.createJoint(rj);
+				rope.joints.addLast(world.createJoint(rj));
 			}
 			
 			prev = segment;
 		}
 		
 		RevoluteJointDef jd = new RevoluteJointDef();
-		jd.initialize(toBody, prev, direction.mul(resolution * i));
+		jd.initialize(toBody, prev, from.add(direction.mul(resolution * i)));
 		jd.collideConnected = false;
-		rope.joints[count * 2 + 1] = world.createJoint(jd);
+		rope.joints.add(world.createJoint(jd));
 		
 		// create the final rope joint between the from and to
 		RopeJointDef rj = new RopeJointDef();
@@ -167,7 +207,7 @@ public class RopeTest extends TestbedTest {
 		rj.localAnchorB.set(toBody.getLocalPoint(to));
 		rj.maxLength = from.sub(to).length();
 		rj.collideConnected = false;
-		rope.joints[count * 2] = world.createJoint(rj);
+		rope.joints.add(world.createJoint(rj));
 		
 		return rope;
 	}
